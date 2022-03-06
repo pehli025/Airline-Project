58134b4bcf59a686212501d5ea4616cecb91dddd
pehli025 <Joshpehling@gmail.com>
2022-03-06 17:36:51 -0600

    Adding comments to the following files: - plane.h - customer.h - check_in_customer.h - airline.h

--- check_in_customer.h
+++ check_in_customer.h
@@ -6,10 +6,19 @@
 #include "customer.h"
 class CheckInCustomer {
  public:
-    static bool CustomerSearch(std::unordered_map<int, Customer*> customers, int custId);
+    /*
+     * @desc Uses the custId to search the customers vector for the key customer.
+     * */
+    static bool CustomerSearch(std::unordered_map<int, Customer*> customers, int custId); //
+    /*
+     * @desc Initializes the user to create a new customer.
+     */
     static void CreateNewCustomer();
+    /*
+     * @desc during the customer check in phase, ChooseFlight will prompt the user to choose a flight from the list of available flights
+     */
     static void ChooseFlight(Customer* cust);
-    void AssignPlane(int custId);
+    //void AssignPlane(int custId);
 
 };
 
